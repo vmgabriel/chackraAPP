6deeccc2d8eb673fd195c731bb39893b26c7f83a
Gabriel Vargas <vmgabriel96@gmail.com>
2025-08-12 14:27:11 -0500

    feat: add syles and colors for app

---
+++ lib/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Color base for Generate the project and Views in General
+
+const Color kPrimaryColor = Color(0xFF0A2342);
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kHintColor = Color(0xFFBDBDBD);
+
+const Color kBackgroundColor = Color(0xFFE2EEE5);
+const Color kAccentColor = Color(0xFF1ABC9C);
